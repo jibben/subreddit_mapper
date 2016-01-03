80c1895bcd7600f03abecafd46e8baba04f7eff4
Jibben Hillen <jlh2218@columbia.edu>
2016-01-03 16:31:05 -0500

    Created config template and updated README

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # subreddit_mapper
 Mapping the structure of subreddits via sidebar recommendations
+
+### Requirements
+config.json file with information filled out (template is provided). You can leave callback_uri as it is.
+To get an ID and secret, go to [reddit's app page](https://www.reddit.com/prefs/apps/).
--- test.py
+++ test.py
@@ -6,7 +6,7 @@ r = praw.Reddit(user_agent='subreddit_mapper see https://github.com/jibbenHillen
 # read in info from config file
 with open('config.json', 'rb') as config_file:
     config = json.load(config_file)
-
+# set up authorization
 r.set_oauth_app_info(client_id=config["client_id"],
                      client_secret=["client_secret"],
                      redirect_uri=["redirect_uri"])
