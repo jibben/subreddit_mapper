2f1003334911e14764b0a55fed990b0dc5ee1ea0
Jibben Hillen <jlh2218@columbia.edu>
2016-01-03 16:23:09 -0500

    bot successfully connecting and requesting from reddit

---
+++ test.py
@@ -0,0 +1,15 @@
+import praw
+import json
+
+# initialize praw object
+r = praw.Reddit(user_agent='subreddit_mapper see https://github.com/jibbenHillen/subreddit_mapper')
+# read in info from config file
+with open('config.json', 'rb') as config_file:
+    config = json.load(config_file)
+
+r.set_oauth_app_info(client_id=config["client_id"],
+                     client_secret=["client_secret"],
+                     redirect_uri=["redirect_uri"])
+submissions = r.get_subreddit('opensource').get_hot(limit=10)
+for thing in submissions:
+    print thing
