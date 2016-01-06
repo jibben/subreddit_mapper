11c0b24133e8d51313ec3f0980924c8ed812325e
Jibben Hillen <jlh2218@columbia.edu>
2016-01-06 02:22:08 -0500

    fixed request URI in setup

--- setup.py
+++ setup.py
@@ -11,10 +11,9 @@ import json
 def main():
     cid     = raw_input('Client ID: ')
     csecret = raw_input('Client Secret: ')
-    ruri    = raw_input('Redirect URI: ')
 
     f = open('config.json', 'w')
-    json.dump({ 'client_id': cid, 'client_secret': csecret, 'redirect_uri': ruri }, f)
+    json.dump({ 'client_id': cid, 'client_secret': csecret, 'redirect_uri': 'http://127.0.0.1:65010/authorize_callback'}, f)
     f.close()
 
 # Primary entry point.
