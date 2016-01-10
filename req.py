74c4e6401f82c6eb39a7bdbb084cf024b001629c
Jibben Hillen <jlh2218@columbia.edu>
2016-01-09 21:53:43 -0500

    changed usage of req to keep track of urls queried

--- req.py
+++ req.py
@@ -3,60 +3,46 @@ import httplib
 
 # this class will return the end url of a link shortener
 # works with multi-level shortening
-class link_lengthener():
+    class link_lengthener():
 
-    # to initialize, parse the given url into parts for later request
-    # undefined behavior for url without server
-    def __init__(self,url):
+    # initialize dictionary to optimize against re-querying
+    def __init__(self):
+        seen = {}
+
+    def extend(self, url):
         # url needs to start with http(s) for parser to work
         if "://" not in url:
             url = "http://" + url
         o = urlparse(url)
-        if o.scheme == 'https':
-            self.use_secure = True
-        elif o.scheme == 'http':
-            self.use_secure = False
+        path = '/' if o.path=='' else o.path
+        # check to see if url has already been queried
+        try:
+            return seen[o.netloc + path]
+        # query server and get response
+        except:
+            r = __get_r(o.scheme, o.netloc, '/' if o.path=='' else o.path)
+            resp_str = ("HTTP/" + str(r.version)[0:1] + "."
+                    + str(r.version)[1:2] +" "+ str(r.status) + " " + r.reason)
+            # build dictionary
+            url_dict = {}
+            url_dict['response'] = resp_str
+            url_dict['url'] = r.getheader('location')
+            url_dict['header'] = r.msg.headers
+            seen[url] = url_dict
+            return url_dict
+
+
+    # private function to get response
+    def __get_r(scheme, server, path):
+        if scheme.upper() == 'HTTPS':
+            self.connection = httplib.HTTPSConnection(self.server, 80)
+        elif scheme.upper() == 'HTTP':
+            self.connection = httplib.HTTPConnection(self.server, 80)
         else:
             raise NotHTTP("Link must be HTTP(S), not " + o.scheme)
-        self.server = o.netloc
-        if o.path == '':
-            self.path = '/'
-        else:
-            self.path = o.path
-        # init empty connection and response for later use
-        self.connection = None
-        self.response = None
-
-    # private function to get response, actual querying is lazy
-    def __get_response(self):
-        if self.use_secure:
-            self.connection = httplib.HTTPSConnection(self.server, 80)
-        else:
-            self.connection = httplib.HTTPConnection(self.server, 80)
         self.connection.request("HEAD", self.path)
         self.response = self.connection.getresponse()
         self.connection.close()
-
-    # get the response message
-    def get_response(self):
-        if self.response is None:
-            self.__get_response()
-        resp_str = ("HTTP/" + str(self.response.version)[0:1] + "."
-                + str(self.response.version)[1:2] + " "
-                + str(self.response.status) + " " + self.response.reason)
-        return resp_str
-
-    # get the end url
-    def get_url(self):
-        if self.response is None:
-            self.__get_response()
-        return self.response.getheader('location')
-
-    # get list of header elements
-    def get_header(self):
-        if self.response is None:
-            self.__get_response()
-        return self.response.msg.headers
 
 # Exception to use in case of not HTTP link
 class NotHTTP(Exception):
