c5d86a50d818dba5cc65e398637774ffda2c2dcf
Jibben Hillen <jlh2218@columbia.edu>
2016-01-12 01:08:09 -0500

    link_lengthener gracefully handles unknown hosts

--- req.py
+++ req.py
@@ -1,5 +1,6 @@
 from urlparse import urlparse
 import httplib
+import socket
 
 # this class will return the end url of a link shortener
 # does not work with multi-level shortening (un-covered border case)
@@ -8,7 +9,7 @@ class link_lengthener():
     # initialize dictionary to optimize against re-querying
     def __init__(self):
         self.seen = {}
-
+        self.no_redirects = {}
     # Parameters:
     # url: string of url to extend
     def extend(self, url):
@@ -21,33 +22,38 @@ class link_lengthener():
         if (o.netloc + path) in self.seen:
             return self.seen[o.netloc + path]
         # else check to see if website has served many unshortened links
-        elif (o.netloc in no_redirects) and no_redirects[o.netloc] == 3:
+        elif (o.netloc in self.no_redirects) and self.no_redirects[o.netloc] == 3:
             url_dict = {}
-            url_dict['response'] = "HTTP/1.1 200 OK
+            url_dict['response'] = "HTTP/1.1 200 OK"
             url_dict['url'] = url
-        # else query server and get response
+        # else try to query server and get response
         else:
-            r = get_r(o.scheme, o.netloc, path)
-            resp_str = ("HTTP/" + str(r.version)[0:1] + "."
-                    + str(r.version)[1:2] +" "+ str(r.status) + " " + r.reason)
-            # build dictionary
             url_dict = {}
-            url_dict['response'] = resp_str
-            # if the status code indicates a re-direct (3xx)
-            if r.status / 100 == 3:
-                url_dict['url'] = r.getheader('location')
-            # if status code indicates OK (1xx or 2xx)
-            elif r.status / 100 in (1,2):
-                url_dict['url'] = o.scheme + "://" + o.netloc + path
-                # keep track of servers that don't redirect
-                if o.netloc in no_redirects:
-                    no_redirects[o.netloc] += 1
+            try:
+                r = get_r(o.scheme, o.netloc, path)
+                resp_str = ("HTTP/" + str(r.version)[0:1] + "."
+                    + str(r.version)[1:2] +" "+ str(r.status) + " " + r.reason)
+                # build dictionary
+                url_dict['response'] = resp_str
+                # if the status code indicates a re-direct (3xx)
+                if r.status / 100 == 3:
+                    url_dict['url'] = r.getheader('location')
+                # if status code indicates OK (1xx or 2xx)
+                elif r.status / 100 in (1,2):
+                    url_dict['url'] = o.scheme + "://" + o.netloc + path
+                    # keep track of servers that don't redirect
+                    if o.netloc in self.no_redirects:
+                        self.no_redirects[o.netloc] += 1
+                    else:
+                        self.no_redirects[o.netloc] = 1
+                # if status code indicates req or server error (4xx or 5xx)
+                # of if server returns invalid status code
                 else:
-                    no_redirects[o.netloc] = 1
-            # if status code indicates req or server error (4xx or 5xx)
-            # of if server returns invalid status code
-            else:
+                    url_dict['url'] = ''
+            # not a valid server
+            except socket.gaierror:
                 url_dict['url'] = ''
+                url_dict['response'] = "unknown host"
 
         # add info to seen and return
         self.seen[o.netloc + path] = url_dict
