dcf8a53956f23a5740c811b7c4980c39dec3e1e2
Jibben Hillen <jlh2218@columbia.edu>
2016-01-12 00:34:48 -0500

    link_lengthener no longer returns header, won't repeatedly query non-shortening sites

--- req.py
+++ req.py
@@ -9,6 +9,8 @@ class link_lengthener():
     def __init__(self):
         self.seen = {}
 
+    # Parameters:
+    # url: string of url to extend
     def extend(self, url):
         # url needs to start with http(s) for parser to work
         if "://" not in url:
@@ -16,26 +18,40 @@ class link_lengthener():
         o = urlparse(url)
         path = '/' if o.path=='' else o.path
         # check to see if url has already been queried
-        try:
+        if (o.netloc + path) in self.seen:
             return self.seen[o.netloc + path]
-        # query server and get response
-        except:
+        # else check to see if website has served many unshortened links
+        elif (o.netloc in no_redirects) and no_redirects[o.netloc] == 3:
+            url_dict = {}
+            url_dict['response'] = "HTTP/1.1 200 OK
+            url_dict['url'] = url
+        # else query server and get response
+        else:
             r = get_r(o.scheme, o.netloc, path)
             resp_str = ("HTTP/" + str(r.version)[0:1] + "."
                     + str(r.version)[1:2] +" "+ str(r.status) + " " + r.reason)
             # build dictionary
             url_dict = {}
             url_dict['response'] = resp_str
-            # if the status code indicates a re-direct
+            # if the status code indicates a re-direct (3xx)
             if r.status / 100 == 3:
                 url_dict['url'] = r.getheader('location')
-            elif r.status / 100 == 4:
-                url_dict['url'] = ''
-            else:
+            # if status code indicates OK (1xx or 2xx)
+            elif r.status / 100 in (1,2):
                 url_dict['url'] = o.scheme + "://" + o.netloc + path
-            url_dict['header'] = r.msg.headers
-            self.seen[o.netloc + path] = url_dict
-            return url_dict
+                # keep track of servers that don't redirect
+                if o.netloc in no_redirects:
+                    no_redirects[o.netloc] += 1
+                else:
+                    no_redirects[o.netloc] = 1
+            # if status code indicates req or server error (4xx or 5xx)
+            # of if server returns invalid status code
+            else:
+                url_dict['url'] = ''
+
+        # add info to seen and return
+        self.seen[o.netloc + path] = url_dict
+        return url_dict
 
 # Exception to use in case of not HTTP link
 class NotHTTP(Exception):
